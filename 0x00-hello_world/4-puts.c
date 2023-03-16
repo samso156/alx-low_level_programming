9a22437ce2bfa2f48b91b3c0734ee55fc8e01330
Samson Barongo <barongo156@gmail.com>
2023-03-16 10:03:29 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ *main - A sample function to print a text
+ *Return : Always 0 for success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
